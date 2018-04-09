eb78319e3033b8db92d08bf116b070648805ff07
Argir Argirov <dj.giro@gmail.com>
2018-04-09 14:28:37 -0400

    add socketio connection event handlers

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const path = require('path')
-const express = require('express');
+const http = require('http');
+const express = require('express')
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 let app = express()
+let server = http.createServer(app)
+let io = socketIO(server)
 
 app.use(express.static(publicPath))
 
-app.get('/', (req, res) => {
-  res.render('index')
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('client disconected');
+  })
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('started on port '+ port);
 })
