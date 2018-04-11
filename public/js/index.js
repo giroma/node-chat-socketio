77012970ee03a706b65debbdb863a44d7540dbd0
Argir Argirov <dj.giro@gmail.com>
2018-04-11 16:06:12 -0400

    scroll to bottom on new messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 let socket = io();
+
+function scrollToBottom() {
+  //selectors
+  let messages = jQuery('#messages')
+  let newMessage = messages.children('li:last-child')
+  //heights
+  let clientHeight = messages.prop('clientHeight')
+  let scrollTop = messages.prop('scrollTop')
+  let scrollHeight = messages.prop('scrollHeight')
+  let newMessageHeight = newMessage.innerHeight()
+  let lastMessageHeight = newMessage.prev().innerHeight()
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function () {
   console.log('connected to server')
 
@@ -17,6 +34,7 @@ socket.on('newMessage', function (message) {
   })
 
   jQuery('#messages').append(html)
+  scrollToBottom()
   // let formattedTime = moment(message.createdAt).format('h:mm a')
   // let li = jQuery('<li></li>')
   // li.text(`${message.from} ${formattedTime}: ${message.text}`)
@@ -34,6 +52,8 @@ socket.on('newLocationMessage', function (message) {
   })
 
     jQuery('#messages').append(html)
+    scrollToBottom()
+
   // let li = jQuery('<li></li>')
   // let a = jQuery('<a target="_blank">My current location</a>')
   //
