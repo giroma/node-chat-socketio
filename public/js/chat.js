c5820f599bafba0497995ab90ace1ff7b3175fb0
Argir Argirov <dj.giro@gmail.com>
2018-04-16 13:56:28 -0400

    messages local to rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -87,7 +87,6 @@ jQuery('#message-form').on('submit', function (e) {
   const messageTextBox = jQuery('[name=message]')
 
   socket.emit('createMessage', {
-    from: 'user',
     text: messageTextBox.val()
   }, function () {
     messageTextBox.val('')
